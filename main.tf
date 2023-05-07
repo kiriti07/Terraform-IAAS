23e135f541584572038e24682a7abda7c6747f33
kiriti07 <ch.kiriti@gmail.com>
2023-05-08 03:22:03 +0530

    Adding new access and secret key

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
 provider "aws" {
-  access_key = "AKIASXQCLADLJXKMPIWO"
-  secret_key = "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"
+  access_key = "AKIASXQCLADLLZNC6ROT"
+  secret_key = "pQy8T1VCizAiEg2f00xIcmZ2fSstgW2/Vtg39gt3"
   region     = "us-east-1"
 }
 
 resource "aws_instance" "example_instance_1" {
-  ami           = ami-0889a44b331db0194
-  instance_type = t2.micro
+  ami           = "ami-0889a44b331db0194"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "Example Instance 1"
   }
+}
