94eda6e6b54e2d24ef96fa62854a4432dba644a8
kiriti07 <ch.kiriti@gmail.com>
2023-05-09 23:51:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-iaac-kiriti"
-    key    = "terraform/terraform.tfstate"
+    key    = "terraform/s3/terraform.tfstate"
     region = "ap-south-1"
     encrypt = true
   }
