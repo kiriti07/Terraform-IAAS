1c2d5f7fc5ccacac1013505225ed4e0648428c12
kiriti07 <ch.kiriti@gmail.com>
2023-05-09 20:32:11 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
-    my_aws_provider = {
+      aws_provider = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
 }
-provider "my_aws_provider" {
+provider "aws_provider" {
   region = "ap-south-1"
 }
