f8c50c3f88e9f1a0a738379059cb66ecd364e905
kiriti07 <ch.kiriti@gmail.com>
2023-05-09 23:17:35 +0530

    Adding S3 and backend

--- main.tf
+++ main.tf
@@ -1,4 +1,30 @@
-resource "aws_instance" "example_instance_1" {
+terraform {
+  backend "s3" {
+    bucket = "terraform-iaac-kiriti"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+    encrypt = true
+  }
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "terraform-iaac-kiriti"
+  lifecycle {
+      prevent_destroy = true
+  }
+  versioning {
+      enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_instance" "App_instance" {
   ami           = "ami-0889a44b331db0194"
   instance_type = "t2.micro"
 
