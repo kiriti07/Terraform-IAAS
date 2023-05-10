3d93a78b064a38c3f56c4081225e31ccf81f1deb
kiriti07 <ch.kiriti@gmail.com>
2023-05-11 01:30:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,34 +7,6 @@ terraform {
     encrypt = true
   }
 }
-
-resource "aws_s3_bucket" "tf_state" {
-  bucket = "terraform-iaac-kiriti"
-  lifecycle {
-      prevent_destroy = true
-  }
-  versioning {
-      enabled = true
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name             = "terraform-state-lock"
-  hash_key         = "LOCKID"
-  billing_mode     = "PAY_PER_REQUEST"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
   
 resource "aws_instance" "App_instance" {
   ami           = "ami-0889a44b331db0194"
