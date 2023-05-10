b1740680dda455335f0f2f90ca8183f728b122d6
kiriti07 <ch.kiriti@gmail.com>
2023-05-11 01:32:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-iaac-kiriti"
-    key    = "terraform/s3/terraform.tfstate"
-    region = "ap-south-1"
-    dynamodb_table = "terraform-state-lock"
-    encrypt = true
-  }
-}
-  
 resource "aws_instance" "App_instance" {
   ami           = "ami-0889a44b331db0194"
   instance_type = "t2.micro"
